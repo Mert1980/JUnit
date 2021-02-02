02d3382ac1a753d52bd3b1ee6afeff171351fe47
Mert <mertdemirok80@gmail.com>
2021-02-02 10:30:40 +0100

    remove import Test

--- opdracht1/src/test/java/be/intecbrussel/param/WordReverserTest.java
+++ opdracht1/src/test/java/be/intecbrussel/param/WordReverserTest.java
@@ -2,7 +2,6 @@ package be.intecbrussel.param;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
